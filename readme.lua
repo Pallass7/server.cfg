5c493b17d255dcdc7ff49c4f31692199bcf753bb
Pallas <urgenceliferpoff@gmail.com>
2026-05-16 01:27:50 +0200

    Create readme.lua

---
+++ readme.lua
@@ -0,0 +1,22 @@
+FREE TU USE
+BY PALLAS
+
+
+
+
+https://github.com/Pallass7/server.cfg
+https://github.com/Pallass7/server.cfg
+https://github.com/Pallass7/server.cfg
+https://github.com/Pallass7/server.cfg
+https://github.com/Pallass7/server.cfg
+https://github.com/Pallass7/server.cfg
+https://github.com/Pallass7/server.cfg
+https://github.com/Pallass7/server.cfg
+https://github.com/Pallass7/server.cfg
+https://github.com/Pallass7/server.cfg
+https://github.com/Pallass7/server.cfg
+https://github.com/Pallass7/server.cfg
+https://github.com/Pallass7/server.cfg
+https://github.com/Pallass7/server.cfg
+https://github.com/Pallass7/server.cfg
+
